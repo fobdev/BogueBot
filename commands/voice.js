6521c80f4efbb80bbb068223c1a189f45ecb7b15
Fobenga <pedro.h.s.s09@hotmail.com>
2018-11-03 14:42:07 -0300

    Update commands/voice.js

--- commands/voice.js
+++ commands/voice.js
@@ -1,11 +1,10 @@
-const Discord = require("Discord.js");
+const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) =>
 {
-
 	if (message.member.voiceChannel && message.guild.voiceConnection)
 	{
-
+		message.member.voiceChannel.join();
 	}
 }
 
