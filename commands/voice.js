1c70cddb0f10a3b9507382e478393b9bba0bde74
Fobenga <pedro.h.s.s09@hotmail.com>
2018-11-03 17:01:17 -0300

    voice command just for testing, not finished

--- commands/voice.js
+++ commands/voice.js
@@ -2,9 +2,24 @@ const Discord = require("discord.js");
 
 module.exports.run = async(bot, message, args) =>
 {
-	if (message.member.voiceChannel && message.guild.voiceConnection)
+	const voice_embed = new Discord.RichEmbed()
+		.setFooter(`Chamado por ${message.author.username}`, message.author.displayAvatarURL);
+
+	if (message.member.voiceChannel && !message.guild.voiceConnection)
 	{
-		message.member.voiceChannel.join();
+		return message.member.voiceChannel.join()
+			.then(connection =>
+			{
+				message.channel.send(voice_embed
+					.setTitle("Entrou no canal com sucesso.")
+					.setColor("#00FF00"));
+			});
+	}
+	else
+	{
+		message.channel.send(voice_embed
+			.setTitle("Você deve estar em um canal de voz.")
+			.setColor("#FF0000"));
 	}
 }
 
