6ee4ebca0bf37340aac2bb8fe2a515d61d1f0c9b
Fobenga <pedro.h.s.s09@hotmail.com>
2018-11-04 17:44:18 -0300

    bugs fixed

--- commands/music.js
+++ commands/music.js
@@ -11,6 +11,63 @@ module.exports.run = async (bot, message, args) =>
 	const voiceChannel = message.member.voiceChannel;
 	const serverQueue = queue.get(message.guild.id);
 	const url = args.join(" ");
+
+	if (!voiceChannel)
+	{
+		return message.channel.send(voice_embed
+			.setTitle("Você não está em um canal de voz.")
+			.setColor("FF0000"));
+	}
+
+
+	var song_info = await ytdl.getInfo(url);
+	var song = {
+		title: song_info.title,
+		url: song_info.video_url
+	};
+
+	if (!serverQueue)
+	{
+		const queueConstruct = {
+			textChannel: message.channel,
+			voiceChannel: voiceChannel,
+			connection: null,
+			songs: [],
+			volume: 5,
+			playing: true
+		};
+
+		queue.set(message.guild.id, queueConstruct);
+
+		queueConstruct.songs.push(song);
+
+		try
+		{
+			var connection = await voiceChannel.join();
+			queueConstruct.connection = connection;
+			play(bot, message, message.guild, queueConstruct.songs[0]);
+
+		}
+		catch (e)
+		{
+			console.log(`Bot could not join a voice channel: + ${e}`);
+
+			queue.delete(message.guild.id);
+
+			return message.channel.send(voice_embed
+				.setTitle("Não foi possível conectar ao canal de voz.")
+				.setColor("#FF0000"));
+		}
+	}
+	else
+	{
+		serverQueue.songs.push(song);
+		console.log(serverQueue.songs);
+		return message.channel.send(voice_embed
+			.setTitle(`**${song.title}** foi adicionado à fila`)
+			.setColor("#00FF00"));
+	}
+
 	if (url === "leave")
 	{
 		if (!voiceChannel)
@@ -21,73 +78,10 @@ module.exports.run = async (bot, message, args) =>
 		}
 		else
 		{
-			await queue.delete(message.guild.id);
 			voiceChannel.leave();
-			return message.channel.send(voice_embed
-				.setTitle("Saindo do canal de voz.")
-				.setColor("#00FF00"));
-		}
-
-	}
-	else
-	{
-
-		if (!voiceChannel)
-		{
-			return message.channel.send(voice_embed
-				.setTitle("Você não está em um canal de voz.")
-				.setColor("FF0000"));
-		}
-
-
-		var song_info = await ytdl.getInfo(url);
-		var song = {
-			title: song_info.title,
-			url: song_info.video_url
-		};
-
-		if (!serverQueue)
-		{
-			const queueConstruct = {
-				textChannel: message.channel,
-				voiceChannel: voiceChannel,
-				connection: null,
-				songs: [],
-				volume: 5,
-				playing: true
-			};
-
-			queue.set(message.guild.id, queueConstruct);
-
-			queueConstruct.songs.push(song);
-
-			try
-			{
-				var connection = await voiceChannel.join();
-				queueConstruct.connection = connection;
-				play(bot, message, message.guild, queueConstruct.songs[0]);
-
-			}
-			catch (e)
-			{
-				console.log(`Bot could not join a voice channel: + ${e}`);
-
-				queue.delete(message.guild.id);
-
-				return message.channel.send(voice_embed
-					.setTitle("Não foi possível conectar ao canal de voz.")
-					.setColor("#FF0000"));
-			}
-		}
-		else
-		{
-			serverQueue.songs.push(song);
-			console.log(serverQueue.songs);
-			return message.channel.send(voice_embed
-				.setTitle(`**${song.title}** foi adicionado à fila`)
-				.setColor("#00FF00"));
 		}
 	}
+
 }
 
 function play(bot, message, guild, song)
