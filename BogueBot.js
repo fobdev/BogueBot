95328e9c54e27bf62d1c270f17543d1ebb398826
Fobenga <pedro.h.s.s09@hotmail.com>
2018-11-05 21:35:29 -0300

    Update BogueBot.js

--- BogueBot.js
+++ BogueBot.js
@@ -131,7 +131,7 @@ bot.on('message', async message => {
     let command_file = bot.commands.get(cmd.slice(prefix.length));
     if (cmd[0] === prefix) {
 
-        console.log(`\nUser '${message.author.username}'` +
+        console.log(`User '${message.author.username}'` +
             ` sent [${message}]\nserver '${message.guild.name}'\nchannel '#${message.channel.name}'\n`);
 
         if (command_file) command_file.run(bot, message, args);
