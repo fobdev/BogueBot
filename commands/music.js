c405c31f40f77c287225746ce563de98da66d191
Fobenga <pedro.h.s.s09@hotmail.com>
2018-11-06 12:59:00 -0300

    ok I cant put song title

--- commands/music.js
+++ commands/music.js
@@ -100,10 +100,9 @@ module.exports.run = async (bot, message, args) => {
 			case "skip":
 				{
 					try {
-						song_info = await ytdl.getInfo(url);
 						dispatcher.end();
 						return message.channel.send(arg_embed
-							.setTitle(`**${song_info.title}** pulado.`));
+							.setTitle(`**${message.author.username}** pulou a reprodução atual.`));
 					} catch (error) {
 						console.log(error);
 						return message.channel.send(arg_embed
