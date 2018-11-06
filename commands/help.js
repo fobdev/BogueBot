b25a70ec22d9ebdb4c97f949e7b6f5d7e192fd3a
Fobenga <pedro.h.s.s09@hotmail.com>
2018-11-06 01:30:15 -0300

    brand new help

--- commands/help.js
+++ commands/help.js
@@ -4,18 +4,18 @@ const botconfig = require("../botconfig.json");
 module.exports.run = async (bot, message, args) => {
 
     const utility_commands =
-        "**|" + `${botconfig.prefix}` + "help  |" +
-        `${botconfig.prefix}` + "clear  |" +
-        `${botconfig.prefix}` + "auth  |**";
+        "**|" + `${botconfig.prefix}` + "help  |**" +
+        `**|${botconfig.prefix}` + "invite |**" + "`` - `` Mostra o link para convidar o BogueBot para qualquer servidor." +
+        `**|${botconfig.prefix}` + "clear [numero] |**" + "`` - `` Apaga uma certa quantidade de mensagens.";
 
     const authority_commands =
-        "**|" + `${botconfig.prefix}` + "mute  |" +
-        `${botconfig.prefix}` + "desmute  |" +
-        `${botconfig.prefix}` + "tempmute  |" +
-        `${botconfig.prefix}` + "kick  |" +
-        `${botconfig.prefix}` + "ban  |" +
-        `${botconfig.prefix}` + "report  |" +
-        `${botconfig.prefix}` + "renameserver  |**";
+        "**|" + `${botconfig.prefix}` + "mute [membro] |**" + "`` - `` Muta um membro do servidor.\n" +
+        `**|${botconfig.prefix}` + "desmute [membro] |**" + "`` - `` Desmuta um membro já mutado no servidor.\n" +
+        `**|${botconfig.prefix}` + "tempmute [membro] [tempo] |**" + "`` - `` Muta temporariamente um membro do servidor.\n" +
+        `**|${botconfig.prefix}` + "kick [membro] [motivo] |**" + "`` - `` Expulsa um membro do servidor.\n" +
+        `**|${botconfig.prefix}` + "ban [membro] [motivo] |**" + "`` - `` Bane um membro do servidor.\n" +
+        `**|${botconfig.prefix}` + "report [membro] [motivo] |**" + "`` - `` Denuncia um membro do servidor para os administradores.\n" +
+        `**|${botconfig.prefix}` + "renameserver [novo nome] |**" + "`` - `` Renomeia o servidor.";
 
     const user_commands =
         "**|" + `${botconfig.prefix}` + "avatar  |**";
@@ -23,11 +23,11 @@ module.exports.run = async (bot, message, args) => {
     const music_file = require("./music.js");
     const musicprefix = `${botconfig.prefix}${music_file.help.name}`;
     const music_commands =
-        `**| ${musicprefix} [url] |**` + "``-`` Toca uma URL do YouTube\n" +
-        `**| ${musicprefix} skip |**` + "``-`` Pula para o próximo video da fila.\n" +
-        `**| ${musicprefix} pause |** ` + "``-`` Pausa o vídeo.\n" +
-        `**| ${musicprefix} play |** ` + "``-`` Continua o vídeo de onde foi pausado.\n" +
-        `**| ${musicprefix} leave |**` + "``-`` Sai do canal de voz e apaga a fila.";
+        `**| ${musicprefix} [url] |**` + "`` - `` Toca uma URL do YouTube\n" +
+        `**| ${musicprefix} skip |**` + "`` - `` Pula para o próximo video da fila.\n" +
+        `**| ${musicprefix} pause |** ` + "`` - `` Pausa o vídeo.\n" +
+        `**| ${musicprefix} play |** ` + "`` - `` Continua o vídeo de onde foi pausado.\n" +
+        `**| ${musicprefix} leave |**` + "`` - `` Sai do canal de voz e apaga a fila.";
 
     let help_embed = new Discord.RichEmbed()
         .setTitle(`${bot.user.username} Ajuda`)
@@ -37,7 +37,7 @@ module.exports.run = async (bot, message, args) => {
         .setTimestamp(bot.user.createdAt)
         .setFooter("Fobenga")
         .setColor("#00FF00")
-        .addField("Musica", music_commands, true)
+        .addField("Musica", music_commands)
         .addField("Usuário", user_commands)
         .addField("Utilidade", utility_commands)
         .addField("Autoridade", authority_commands);
@@ -47,83 +47,6 @@ module.exports.run = async (bot, message, args) => {
         .setTitle(`Lista de comandos do ${bot.user.username}`)
         .setColor("#00FF00");
 
-    switch (helpcommand) {
-        case "help":
-            return message.channel.send(commands_embed.addField(`${botconfig.prefix}${this.help.name}`,
-                `Mostra todos os comandos disponíveis do ${bot.user.username} até o momento.`));
-        case "avatar":
-            {
-                const file = require("./avatar.js")
-                return message.channel.send(commands_embed.addField(`${botconfig.prefix}${file.help.name}`,
-                    "Exibe o avatar em tamanho grande do membro marcado\n" +
-                    `Uso: ${botconfig.prefix}${file.help.name} [@membro]`));
-            }
-        case "auth":
-            {
-                const file = require("./auth.js")
-                return message.channel.send(commands_embed.addField(`${botconfig.prefix}${file.help.name}`,
-                    `Envia um link para convidar o ${bot.name} para qualquer servidor.`));
-            }
-        case "clear":
-            {
-                const file = require("./clear.js")
-                return message.channel.send(commands_embed.addField(`${botconfig.prefix}${file.help.name}`,
-                    "Exclui um certo número de mensagens do canal que foi enviado o comando.\n" +
-                    "No mínimo 1 mensagem e no máximo 100 mensagens podem ser excluídas por vez.\n" +
-                    "**Uso: ``" + `${botconfig.prefix}${file.help.name} [numero de mensagens]` + "``**"));
-            }
-        case "report":
-            {
-                const file = require("./report.js")
-                return message.channel.send(commands_embed.addField(`${botconfig.prefix}${file.help.name}`,
-                    "Reporta um usuário\n**Uso: ``" + `${botconfig.prefix}${file.help.name} [@usuário] [motivo]` + "``"));
-            }
-        case "renameserver":
-            {
-                const file = require("./renameserver.js")
-                return message.channel.send(commands_embed.addField(`${botconfig.prefix}${file.help.name}`,
-                    "Renomeia o servidor, é possível apenas criar nomes entre 2 a 100 caracteres. |" +
-                    " **(apenas para administradores)**\n" +
-                    "**Uso: ``" + `${botconfig.prefix}${file.help.name} [novo nome]` + "``**"));
-            }
-        case "kick":
-            {
-                const file = require("./kick.js")
-                return message.channel.send(commands_embed.addField(`${botconfig.prefix}${file.help.name}`,
-                    "Expulsa um usuário do servidor | **(apenas para administradores)**\n" +
-                    "**Uso:``" + `${botconfig.prefix}${file.help.name} [@usuário] [motivo]` + "``**"));
-            }
-        case "ban":
-            {
-                const file = require("./ban.js")
-                return message.channel.send(commands_embed.addField(`${botconfig.prefix}${file.help.name}`,
-                    "Banir usuário | **(apenas para administradores)**\n" +
-                    "**Uso:``" + `${botconfig.prefix}${file.help.name} [@usuário] [motivo]` + "``**"));
-            }
-        case "tempmute":
-            {
-                const file = require("./tempmute.js")
-                return message.channel.send(commands_embed.addField(`${botconfig.prefix}${file.help.name}`,
-                    "Silenciar um usuário temporariamente | **(apenas para administradores)**\n" +
-                    "**Uso:``" + `${botconfig.prefix}${file.help.name} [@usuário] [tempo](s/m/h)` + "``**"));
-            }
-        case "desmute":
-            {
-                const file = require("./desmute.js")
-                return message.channel.send(commands_embed.addField(`${botconfig.prefix}${file.help.name}`,
-                    "Desmuta o usuário, apenas utilizável caso o usuário já estiver mutado | **(apenas para administradores)**\n" +
-                    "**Uso:``" + `${botconfig.prefix}${file.help.name} [@usuário]` + "``**"));
-            }
-        case "mute":
-            {
-                const file = require("./mute.js")
-                return message.channel.send(commands_embed.addField(`${botconfig.prefix}${file.help.name}`,
-                    "Silenciar um usuário | **(apenas para administradores)**\n" +
-                    "**Uso:``" + `${botconfig.prefix}${file.help.name} [@usuário]` + "``**"));
-            }
-        default:
-            break;
-    }
     return message.channel.send(help_embed);
 }
 
