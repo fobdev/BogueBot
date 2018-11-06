2fa35d5d4b184c69f1db6dcefac7ffe4273002ff
Fobenga <pedro.h.s.s09@hotmail.com>
2018-11-06 02:10:08 -0300

    completely reformulated help file

--- commands/help.js
+++ commands/help.js
@@ -4,37 +4,40 @@ const botconfig = require("../botconfig.json");
 module.exports.run = async (bot, message, args) => {
 
     const utility_commands =
-        "**|" + `${botconfig.prefix}` + "help  |**" +
-        `**|${botconfig.prefix}` + "invite |**" + "`` - `` Mostra o link para convidar o BogueBot para qualquer servidor." +
-        `**|${botconfig.prefix}` + "clear [numero] |**" + "`` - `` Apaga uma certa quantidade de mensagens.";
+        "|``" + `${botconfig.prefix}` + "help             ``|" + "\n" +
+        "|``" + `${botconfig.prefix}` + "invite           ``|" + "`` - `` **Mostra o link para convidar o BogueBot para qualquer servidor.\n**" +
+        "|``" + `${botconfig.prefix}` + "clear [numero]   ``|" + "`` - `` **Apaga uma certa quantidade de mensagens.                        **";
 
     const authority_commands =
-        "**|" + `${botconfig.prefix}` + "mute [membro] |**" + "`` - `` Muta um membro do servidor.\n" +
-        `**|${botconfig.prefix}` + "desmute [membro] |**" + "`` - `` Desmuta um membro já mutado no servidor.\n" +
-        `**|${botconfig.prefix}` + "tempmute [membro] [tempo] |**" + "`` - `` Muta temporariamente um membro do servidor.\n" +
-        `**|${botconfig.prefix}` + "kick [membro] [motivo] |**" + "`` - `` Expulsa um membro do servidor.\n" +
-        `**|${botconfig.prefix}` + "ban [membro] [motivo] |**" + "`` - `` Bane um membro do servidor.\n" +
-        `**|${botconfig.prefix}` + "report [membro] [motivo] |**" + "`` - `` Denuncia um membro do servidor para os administradores.\n" +
-        `**|${botconfig.prefix}` + "renameserver [novo nome] |**" + "`` - `` Renomeia o servidor.";
+        "|``" + `${botconfig.prefix}` + "mute [membro]             ``|" + "`` - `` **Muta um membro do servidor.\n                **" +
+        "|``" + `${botconfig.prefix}` + "desmute [membro]          ``|" + "`` - `` **Desmuta um membro já mutado no servidor.\n   **" +
+        "|``" + `${botconfig.prefix}` + "tempmute [membro] [tempo] ``|" + "`` - `` **Muta temporariamente um usuário.\n           **" +
+        "|``" + `${botconfig.prefix}` + "kick [membro] [motivo]    ``|" + "`` - `` **Expulsa um membro do servidor.\n             **" +
+        "|``" + `${botconfig.prefix}` + "ban [membro] [motivo]     ``|" + "`` - `` **Bane um membro do servidor.\n                **" +
+        "|``" + `${botconfig.prefix}` + "report [membro] [motivo]  ``|" + "`` - `` **Denuncia um membro do servidor.\n            **" +
+        "|``" + `${botconfig.prefix}` + "renameserver [novo nome]  ``|" + "`` - `` **Renomeia o servidor.                         **";
 
     const user_commands =
-        "**|" + `${botconfig.prefix}` + "avatar  |**";
+        "|``" + `${botconfig.prefix}` + "avatar [membro]  ``|" + "`` - `` **Exibe em tamanho grande o avatar de um membro.**";
 
     const music_file = require("./music.js");
     const musicprefix = `${botconfig.prefix}${music_file.help.name}`;
     const music_commands =
-        `**| ${musicprefix} [url] |**` + "`` - `` Toca uma URL do YouTube\n" +
-        `**| ${musicprefix} skip |**` + "`` - `` Pula para o próximo video da fila.\n" +
-        `**| ${musicprefix} pause |** ` + "`` - `` Pausa o vídeo.\n" +
-        `**| ${musicprefix} play |** ` + "`` - `` Continua o vídeo de onde foi pausado.\n" +
-        `**| ${musicprefix} leave |**` + "`` - `` Sai do canal de voz e apaga a fila.";
+        "|``" + `${musicprefix}` + "[url]   ``|" + "`` - `` **Toca uma URL do YouTube\n              **" +
+        "|``" + `${musicprefix}` + "skip    ``|" + "`` - `` **Pula para o próximo video da fila.\n   **" +
+        "|``" + `${musicprefix}` + "pause   ``|" + "`` - `` **Pausa o vídeo.\n                       **" +
+        "|``" + `${musicprefix}` + "play    ``|" + "`` - `` **Continua o vídeo de onde foi pausado.\n**" +
+        "|``" + `${musicprefix}` + "leave   ``|" + "`` - `` **Sai do canal de voz e apaga a fila.    **";
 
     let help_embed = new Discord.RichEmbed()
-        .setTitle(`${bot.user.username} Ajuda`)
-        .setDescription(`Mostra todos os comandos disponíveis do ${bot.user.username} até o momento.` +
-            "\n**Use ``" + `${botconfig.prefix}help [comando] ` + "`` para informação detalhada sobre o comando.**")
+        .setTitle(`**${bot.user.username} Ajuda**`)
+        .setDescription(`Estes são todos os comandos disponíveis do ${bot.user.username} até o momento.\n` +
+            "Note que o Bot está em constante manutenção para testes e implementações de novas funções.\n" +
+            "Bot desenvolvido por [Fobenga](https://github.com/pedroxvi), de código aberto disponível em seu [GitHub](https://github.com/pedroxvi/BogueBot).\n" +
+            "Use o prefixo ``" + `${botconfig.prefix}` + "`` para executar os comandos abaixo.")
         .setAuthor(`Comandos do ${bot.user.username}`, bot.user.displayAvatarURL)
         .setTimestamp(bot.user.createdAt)
+        .setURL("https://github.com/pedroxvi")
         .setFooter("Fobenga")
         .setColor("#00FF00")
         .addField("Musica", music_commands)
