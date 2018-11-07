6c3cbc14bc1d96f404560125344701b370e009f0
Francisco Ramon <franciscoramonsp@outlook.com>
2018-11-07 14:56:42 -0300

    clean debug

--- core/helper.js
+++ core/helper.js
@@ -9,7 +9,6 @@ module.exports.loadKeys = (key) => {
 
     if( fs.existsSync(token_file_name) ){
         var token_file = require.main.require(token_file_name);
-        console.log(token_file);
         return token_file[key];
     }
     throw new Error("Error load Keys");
