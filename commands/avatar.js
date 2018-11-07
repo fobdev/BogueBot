f9195ceedb3eb02532a825c4506da7b56b727694
Fobenga <pedro.h.s.s09@hotmail.com>
2018-11-07 14:43:41 -0300

    now shows server icon with avatar [server]

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 	let user_avatar = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-
+	let sv_icon = args.join(" ");
 	const avatar_embed = new Discord.RichEmbed()
 		.setFooter(`Requisitado por ${message.author.username}`, message.author.displayAvatarURL);
 
@@ -11,6 +11,13 @@ module.exports.run = async (bot, message, args) => {
 				`**${message.guild.name}**`)
 			.setColor("#FF0000"));
 	}
+
+	if (sv_icon === 'server') {
+		return message.channel.send(avatar_embed.setTitle(`Ícone do servidor ${message.guild.name}`)
+			.setImage(message.guild.iconURL)
+			.setColor("#00FF00"));
+	}
+
 
 	return message.channel.send(avatar_embed.setTitle(`Avatar de **${user_avatar.user.username}**`)
 		.setImage(user_avatar.user.displayAvatarURL)
