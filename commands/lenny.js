5414a5a2c511f52540ab44454637d87a1e1ffcb1
Fobenga <pedro.h.s.s09@hotmail.com>
2018-11-08 12:27:15 -0300

    now it's actually random

--- commands/lenny.js
+++ commands/lenny.js
@@ -1,9 +1,11 @@
 const Discord = require("discord.js");
 
-var entry = ['( ͡° ͜ʖ ͡°)', '( ͠° ͟ʖ ͡°)', '( ͡~ ͜ʖ ͡°)', '( ͡ʘ ͜ʖ ͡ʘ)', '( ͡o ͜ʖ ͡o)', '(° ͜ʖ °)', '( ‾ʖ̫‾)', '( ಠ ͜ʖಠ)', '( ͡° ʖ̯ ͡°)', '( ͡ಥ ͜ʖ ͡ಥ)', '༼ ͡° ͜ʖ ͡° ༽', '(·̿Ĺ̯·̿ ̿)'];
-var rand_val = entry[Math.floor(Math.random() * entry.length)];
+const lennys = ['( ͡° ͜ʖ ͡°)', '( ͠° ͟ʖ ͡°)', '( ͡~ ͜ʖ ͡°)', '( ͡ʘ ͜ʖ ͡ʘ)', '( ͡o ͜ʖ ͡o)', '(° ͜ʖ °)', '( ‾ʖ̫‾)', '( ಠ ͜ʖಠ)', '( ͡° ʖ̯ ͡°)', '( ͡ಥ ͜ʖ ͡ಥ)', '༼ ͡° ͜ʖ ͡° ༽', '(·̿Ĺ̯·̿ ̿)'];
 module.exports.run = async (bot, message, args) => {
-    console.log(`${message.author.name} got a lenny face`);
+    var rand_num = Math.floor(Math.random() * lennys.length);
+    var rand_val = lennys[rand_num];
+
+    console.log(`${message.author} got a lenny number [${rand_num}]`);
 
     return message.channel.send(rand_val);
 }
