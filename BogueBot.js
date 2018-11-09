3c9583fb51e44641eeb950021637936ad3cfe918
Fobenga <pedro.h.s.s09@hotmail.com>
2018-11-09 18:54:27 -0300

    last remove of copycat

--- BogueBot.js
+++ BogueBot.js
@@ -182,8 +182,7 @@ bot.on('message', async message => {
     let command_file = bot.commands.get(cmd.slice(prefix.length));
     if (cmd[0] === prefix) {
         console.log(`\nUser [${message.author.username}] sent [${message}]\nserver: [${message.guild.name}]\nchannel: #${message.channel.name}`)
-        console.log(`${bot.user.username} is copycating: ${copycat_switch}`)
-
+        // console.log(`${bot.user.username} is copycating: ${copycat_switch}`)
         if (command_file) command_file.run(bot, message, args);
     }
 });
