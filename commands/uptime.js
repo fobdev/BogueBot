757c6b6a6b687c7cba7de9bd75a9c20b0242e413
Pedro Henrique <pedro.h.s.s09@hotmail.com>
2018-11-09 11:22:48 -0200

    pluralization of names

--- commands/uptime.js
+++ commands/uptime.js
@@ -7,8 +7,16 @@ function ms_convert(ms){
     hoursms=ms % (60*60*1000);
     minutes = Math.floor((hoursms)/(60*1000));
     minutesms=ms % (60*1000);
-    sec = Math.floor((minutesms)/(1000));
-    return "**" + days + " dias, " + hours + " horas e " + minutes + " minutos **";
+    
+    var day_string = 'day';
+    var hour_string = 'hour';
+    var minute_string = 'minute';
+    
+    if(days > 1 || days === 0) day_string = 'days';
+    if(hours > 1 || hours === 0) hour_string = 'hours';
+    if(minutes > 1 || minutes === 0) minute_string = 'minutes';
+    
+    return `**${days} ${day_string}, ${hours} ${hour_string}, ${minutes} ${minute_string}.**`;
 }
 
 module.exports.run = async (bot,message,args) => {
