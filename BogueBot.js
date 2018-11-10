7c75a60de2c1b87c248ccc13114c04c0aa0b76cd
Pedro Henrique <pedro.h.s.s09@hotmail.com>
2018-11-10 17:58:13 -0300

    Update BogueBot.js

--- BogueBot.js
+++ BogueBot.js
@@ -183,7 +183,7 @@ bot.on('message', async message => {
 
     let command_file = bot.commands.get(cmd.slice(prefix.length));
     if (cmd[0] === prefix) {
-        return message.channel.send('O bot está em manutenção no momento');
+        
         if (command_file) command_file.run(bot, message, args);
         console.log(`\nUser [${message.author.username}] sent [${message}]\nserver: [${message.guild.name}]\nchannel: #${message.channel.name}`)
         console.log(`${bot.user.username} is copycating: ${copycat_switch}`)
