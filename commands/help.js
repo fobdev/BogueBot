dd81b48f2a35cf8dd84815da70be011ff31d47dd
Fobenga <pedro.h.s.s09@hotmail.com>
2018-11-12 21:47:02 -0300

    reformulated help / music help

--- commands/help.js
+++ commands/help.js
@@ -1,15 +1,54 @@
 const Discord = require("discord.js");
 const botconfig = require("../botconfig.json");
+const fs = require("fs");
+
+//file_array = new Discord.Collection();
+//fs.readdir('.', (err, files) => {
+//    if (err) console.error(err);
+//    let jsfile = files.filter(f => f.split(".").pop() === "js");
+//    if (jsfile.length < 1) throw new Error("Could not find commands.");
+//
+//    jsfile.forEach((f, i) => {
+//        let props = require(`commands/${f}`);
+//        file_array.set(props.help.descr, props);
+//    });
+//});
+
 
 module.exports.run = async (bot, message, args) => {
+    //  let command_file = file_array.get(message.content.slice(prefix.length + this.help.name));
+    //
+    //  console.log(command_file);
 
-    const utility_commands =
-        "|``" + `${botconfig.prefix}` + "invite           ``|" + "`` - `` **Mostra o link para convidar o " + `${bot.user.username}` + " para qualquer servidor.\n**" +
-        "|``" + `${botconfig.prefix}` + "help             ``|" + "`` - `` **Todos os comandos disponíveis.**\n" +
-        "|``" + `${botconfig.prefix}` + "h                ``|\n" +
-        "|``" + `${botconfig.prefix}` + "help music       ``|" + "`` - `` **Todos os comandos de música disponíveis.**\n" +
-        "|``" + `${botconfig.prefix}` + "serverinfo       ``|" + "`` - `` **Exibe todas as informações do servidor.**\n" +
-        "|``" + `${botconfig.prefix}` + "uptime           ``|" + "`` - `` **Mostra o tempo que o bot está online desde a ultima atualização.**\n";
+
+    const music_commands_small =
+        "[``(m)usic                    ``]";
+
+    const new_music_commands = "```css\n" +
+        `[Comandos de música do ${bot.user.username}]
+
+>music [música]...Toca um vídeo do YouTube / adiciona à fila.
+>music (q)ueue....Exibe toda a fila do servidor.
+       (q)ueue [numero]............Pula para uma certa posição da fila.
+       (q)ueue (del)ete [numero]...Exclui um certo item da fila.
+       (q)ueue purge...............Limpa todos os itens da fila.
+       
+>music np.........Mostra informações sobre o que está sendo tocado.
+>music skip.......Pula a reprodução atual.
+>music p..........Pausa ou despausa a reprodução atual.
+>music earrape....Aumenta extremamente o volume da reprodução atual.
+>music (l)eave....Sai do canal de voz e exclui a fila atual.` +
+        "```";
+
+    const authority_commands_small =
+        "[``mute                ``, " +
+        "`` desmute             ``, " +
+        "`` tempmute            ``, " +
+        "`` kick                ``, " +
+        "`` ban                 ``, " +
+        "`` report              ``, " +
+        "`` renameserver (rs)   ``, " +
+        "`` clear               ``]";
 
     const authority_commands =
         "|``" + `${botconfig.prefix}` + "mute [membro]             ``|" + "`` - `` **Muta um membro do servidor.\n                **" +
@@ -22,46 +61,46 @@ module.exports.run = async (bot, message, args) => {
         "|``" + `${botconfig.prefix}` + "rs [novo nome]            ``|                                                             \n" +
         "|``" + `${botconfig.prefix}` + "clear [numero]            ``|" + "`` - `` **Apaga uma certa quantidade de mensagens.     **";
 
+    const boguebot_commands_small =
+        "[``invite          ``, " +
+        "``help             ``, " +
+        "``serverinfo       ``, " +
+        "``uptime           ``]";
+
+    const boguebot_commands =
+        "|``" + `${botconfig.prefix}` + "invite           ``|" + "`` - `` **Mostra o link para convidar o " + `${bot.user.username}` + " para qualquer servidor.\n**" +
+        "|``" + `${botconfig.prefix}` + "help             ``|" + "`` - `` **Todos os comandos disponíveis.**\n" +
+        "|``" + `${botconfig.prefix}` + "serverinfo       ``|" + "`` - `` **Exibe todas as informações do servidor.**\n" +
+        "|``" + `${botconfig.prefix}` + "uptime           ``|" + "`` - `` **Mostra o tempo que o bot está online desde a ultima atualização.**\n";
+
+    const user_commands_small =
+        "[``avatar    ``]";
+
     const user_commands =
         "|``" + `${botconfig.prefix}` + "avatar [membro]    ``|" + "`` - `` **Exibe em tamanho grande o avatar de um membro.**";
 
+    const fun_commands_small =
+        "[``lenny              ``, " +
+        "``copycat            ``]";
+
     const fun_commands =
-        "|``" + `${botconfig.prefix}` + "lenny              ``|" +
-        "|``" + `${botconfig.prefix}` + "copycat            ``|";
-
-    const music_file = require("./music.js");
-    const musicprefix = `${botconfig.prefix}${music_file.help.name}`;
-
-    const music_commands =
-        "|``" + `${musicprefix}` + " [search]                      ``| " + "`` - `` **Toca um vídeo do YouTube / adiciona à fila.\n              **" +
-        "|``" + `${botconfig.prefix}${music_file.help.name_2} [search]|` + "``\n" +
-        "|``" + `${botconfig.prefix}${music_file.help.name_3} [search]|` + "``\n" +
-        "|``" + `${musicprefix}` + " queue                         ``| " + "``" + ` ou |${musicprefix}` + " q    ``|" + "`` - `` **Visualiza toda a fila.\n   **" +
-        "|``" + `${musicprefix}` + " queue [numero]                ``| " + "`` - `` **Pula para uma certa posição da fila.\n   **" +
-        "|``" + `${musicprefix}` + " queue purge                   ``| " + "`` - `` **Exclui todos os itens da fila.\n   **" +
-        "|``" + `${musicprefix}` + " np                            ``| " + "`` - `` **Exibe o que está sendo tocado no momento.\n   **" +
-        "|``" + `${musicprefix}` + " skip                          ``| " + "``" + ` ou |${musicprefix}` + " s    ``|" + "`` - `` **Pula para o próximo video da fila.\n   **" +
-        "|``" + `${musicprefix}` + " leave                         ``| " + "``" + ` ou |${musicprefix}` + " l    ``|" + "`` - `` **Sai do canal de voz e apaga a fila.    **\n" +
-        "|``" + `${musicprefix}` + " p                             ``| " + "`` - `` **Pausa/despausa a reprodução atual\n**" +
-        "|``" + `${musicprefix}` + " earrape                       ``| " + "`` - `` **Aumenta extremamente o volume fazendo a reprodução ficar inaudível.\n**";
+        "|``" + `${botconfig.prefix}` + "lenny              ``| " + "`` - `` **Lenny face.\n**" +
+        "``" + `${botconfig.prefix}` + "copycat            ``|" + "`` - `` **O comando mais irritante de todos.**";
 
     let help_embed = new Discord.RichEmbed()
-        .setTitle(`**${bot.user.username} Ajuda**`)
-        .setDescription(`Estes são todos os comandos disponíveis do ${bot.user.username} até o momento.\n` +
-            "Note que o Bot está em constante manutenção para testes e implementações de novas funções.\n" +
-            "Bot desenvolvido por <@244270921286811648>, de código aberto disponível em seu [GitHub](https://github.com/pedroxvi/BogueBot).\n" +
-            "Use o prefixo ``" + `${botconfig.prefix}` + "`` para executar os comandos abaixo.\n" +
-            "Você também pode usar ``" + `${botconfig.prefix}${this.help.name}` + " [categoria]`` para a ajuda de uma categoria específica da lista.")
+        .setDescription(`Esses são todos os comandos que eu sei até o momento.\nEstou em constante atualização, então novos comandos poderão surgir em breve.`)
         .setAuthor(`Comandos do ${bot.user.username}`, bot.user.displayAvatarURL)
         .setTimestamp(bot.user.createdAt)
         .setURL("https://github.com/pedroxvi")
-        .setFooter("Fobenga, criado em ")
+        .setFooter("Fobenga, criado em ", 'https://images-ext-1.discordapp.net/external/HRRbNejI4Jdna8UcivhiBDfEj382i4-yPwkArneYpLU/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/244270921286811648/09318e9b9103e6806fa74258f414394c.png')
         .setColor("#00FF00")
-        .addField('\u200B', "**__UTIL__**\n" + utility_commands)
-        .addField('\u200B', "**__MUSIC__**\n" + music_commands)
-        .addField('\u200B', "**__ADMIN__**\n" + authority_commands)
-        .addField('\u200B', "**__USER__**\n" + user_commands)
-        .addField('\u200B', "**__FUN__**\n" + fun_commands);
+        .addField("MUSIC", music_commands_small)
+        .addField(bot.user.username.toUpperCase(), boguebot_commands_small)
+        .addField("ADMIN", authority_commands_small)
+        .addField("USER", user_commands_small)
+        .addField("FUN", fun_commands_small)
+        .addField('\u200B', "**Use ``" + `${botconfig.prefix}${this.help.name} [categoria]` + "`` para informação detalhada sobre uma categoria**")
+        .addField('Exemplos', "``" + `${botconfig.prefix}${this.help.name} music` + "`` exibe todos os comandos de música");
 
     let helpcommand = args.join(" ");
     let subhelp_embed = new Discord.RichEmbed()
@@ -72,27 +111,31 @@ module.exports.run = async (bot, message, args) => {
     switch (helpcommand) {
         case 'music':
             {
-                return message.channel.send(subhelp_embed
-                    .setTitle(`${bot.user.username} Comandos do Music Player`)
-                    .setDescription(music_commands));
+                return message.channel.send(new_music_commands);
             }
         case 'admin':
             {
                 return message.channel.send(subhelp_embed
-                    .setTitle(`${bot.user.username} Comandos administrativos`)
+                    .setTitle(`Comandos Administrativos`)
                     .setDescription(authority_commands));
             }
         case 'util':
             {
                 return message.channel.send(subhelp_embed
-                    .setTitle(`${bot.user.username} Comandos Úteis`)
-                    .setDescription(utility_commands));
+                    .setTitle(`${bot.user.username}`)
+                    .setDescription(boguebot_commands));
             }
         case 'user':
             {
                 return message.channel.send(subhelp_embed
-                    .setTitle(`${bot.user.username} Comandos de usuário`)
+                    .setTitle(`Comandos de Usuário`)
                     .setDescription(user_commands));
+            }
+        case 'fun':
+            {
+                return message.channel.send(subhelp_embed
+                    .setTitle(`Aleatórios`)
+                    .setDescription(fun_commands));
             }
         default:
             break;
@@ -103,5 +146,5 @@ module.exports.run = async (bot, message, args) => {
 
 module.exports.help = {
     name: "help",
-    name_2: 'h'
+    name_2: 'h',
 }
