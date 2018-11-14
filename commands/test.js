335511752eb4498b232b45ca3e3346ba84d4ed56
Fobenga <pedro.h.s.s09@hotmail.com>
2018-11-14 12:06:57 -0300

    commented test code

--- commands/test.js
+++ commands/test.js
@@ -1,38 +1,48 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-
-	//var pages_embed = new Discord.RichEmbed()
-	//	.setTitle('This is a embed.')
-	//	.setColor("#00FF00");
-	//
-	//const react_controls = {
-	//	NEXT: '▶',
-	//	PREV: '◀'
-	//}
-	//
-	//const react_collector = new Discord.ReactionCollector(message, (reaction, user) => Object.values(react_controls).includes(reaction.emoji.name))
-	//
-	//
-	//react_collector.on('collect', (reaction, user) => {
-	//	var page_number = 0;
-	//	switch (reaction.emoji.name) {
-	//		case emojis.NEXT:
-	//			{
-	//				page_number++;
-	//				pages_embed.title = `Page ${page_number}`
-	//			}
-	//			break;
-	//		case emojis.PREV:
-	//			{
-	//				page_number--;
-	//				pages_embed.title = `Page ${page_number}`
-	//			}
-	//			break;
-	//	}
-	//})
-	//
-	//message.channel.send(pages_embed);
+	// const react_controls = {
+	// 	NEXT: '▶',
+	// 	PREV: '◀'
+	// }
+	// 
+	// const reactionFilter = (reaction, user) => Object.values(react_controls).includes(reaction.emoji.name);
+	// 
+	// const init_embed = new Discord.RichEmbed()
+	// 	.setTitle('This embed will be edited')
+	// 	.setDescription('THIS WILL CHANGE: ')
+	// 	.setColor('#00FF00');
+	// 
+	// // add reaction emoji to message
+	// message.channel.send(init_embed)
+	// 	.then(msg => msg.react('◀'))
+	// 	.then(mReaction => mReaction.message.react('▶'))
+	// 	.then(mReaction => {
+	// 		const collector = mReaction.message
+	// 			.createReactionCollector(reactionFilter)
+	// 
+	// 		var number = 0;
+	// 		collector.on('collect', r => {
+	// 
+	// 			if (r.emoji.name === react_controls.NEXT) {
+	// 				number++;
+	// 				console.log(number);
+	// 			} else {
+	// 				number--
+	// 				console.log(number);
+	// 			}
+	// 
+	// 			let descript = init_embed.description;
+	// 			descript = `NUMBER: ${number}`;
+	// 
+	// 			const new_embed = new Discord.RichEmbed()
+	// 				.setTitle(init_embed.title)
+	// 				.setDescription(descript);
+	// 
+	// 			r.message.edit(new_embed);
+	// 		});
+	// 	})
+	// 
 }
 
 module.exports.help = {
