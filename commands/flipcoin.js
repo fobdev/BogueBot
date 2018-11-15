5de6c5a98a4f1c3f7115846d92b5367452914493
Fobenga <pedro.h.s.s09@hotmail.com>
2018-11-15 18:16:32 -0300

    new flipcoin command

---
+++ commands/flipcoin.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    var coin = Math.floor(Math.random() * 20);
+    var coin_string = '';
+
+    if (coin % 3 === 0)
+        coin_string += "**cara**";
+    else
+        coin_string += "**coroa**";
+
+    return message.channel.send(`<@${message.author.id}> jogou a moeda e caiu em ${coin_string}`);
+}
+
+module.exports.help = {
+    name: 'coinflip',
+    name_2: 'flipcoin'
+}
