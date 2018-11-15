207250f491f37495ac440e0e7fb99ea8b2a5d442
Fobenga <pedro.h.s.s09@hotmail.com>
2018-11-15 18:16:24 -0300

    add support for more than 1 dice

--- commands/dice.js
+++ commands/dice.js
@@ -1,9 +1,34 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-    var dice = Math.floor(Math.random() * 6) + 1;
 
-    return message.channel.send(`<@${message.author.id}> jogou o dado e parou em **${dice}**`);
+    if (args[0]) {
+        if (args[0] < 1) return message.channel.send('Numero de dados inválido.');
+
+        var dices = new Array();
+        try {
+            dices.length = parseInt(args[0]);
+        } catch (e) {
+            return message.channel.send('Numero de dados inválido.');
+        }
+
+        if (dices.length > 5)
+            return message.channel.send('Você só pode jogar no máximo 5 dados por vez.');
+
+        var dice_string = '';
+        for (let i = 0; i < dices.length; i++) {
+            dices[i] = Math.floor(Math.random() * 6) + 1;
+            dice_string += `Dado ${i + 1}: **${dices[i]}**\n`;
+        }
+
+        return message.channel.send(new Discord.RichEmbed()
+            .addField('Resultado dos dados jogados', dice_string)
+            .setColor("#00FF00"));
+
+    } else {
+        var dice = Math.floor(Math.random() * 6) + 1;
+        return message.channel.send(`<@${message.author.id}> jogou o dado e parou em **${dice}**`);
+    }
 }
 
 module.exports.help = {
