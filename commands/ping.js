f235bfa4e99385e039b42cc9740f4da63306b0fa
Fobenga <pedro.h.s.s09@hotmail.com>
2018-12-01 00:41:15 -0300

    created a ping command

---
+++ commands/ping.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) =>
+{
+  return message.channel.send(new Discord.RichEmbed()
+    .setDescription(`**${bot.user.username} Ping: ${bot.ping}ms**`)
+    .setColor("#00FF00"));
+}
+
+module.exports.help = {
+  name: 'ping'
+}
