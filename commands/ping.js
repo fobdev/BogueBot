75d285c00d3807f3865ed6fdb015a26777123f9e
Fobenga <pedro.h.s.s09@hotmail.com>
2018-12-01 00:45:14 -0300

    moduled ping

--- commands/ping.js
+++ commands/ping.js
@@ -2,11 +2,11 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) =>
 {
-  return message.channel.send(new Discord.RichEmbed()
-    .setDescription(`**${bot.user.username} Ping: ${bot.ping}ms**`)
-    .setColor("#00FF00"));
+	return message.channel.send(new Discord.RichEmbed()
+		.setDescription(`**${bot.user.username} Ping: ${Math.floor(bot.ping)}ms**`)
+		.setColor("#00FF00"));
 }
 
 module.exports.help = {
-  name: 'ping'
+	name: 'ping'
 }
