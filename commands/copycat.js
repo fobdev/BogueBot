31e70cafda8476ed38166d1b629327a94132e679
Fobenga <pedro.h.s.s09@hotmail.com>
2018-12-01 12:52:58 -0300

    using copycat only for testing

--- commands/copycat.js
+++ commands/copycat.js
@@ -1,29 +1,40 @@
 const Discord = require('discord.js');
 const botconfig = require('../botconfig.json');
 
+function col_recursive(message, first, collector) {
+    if (first)
+        message.channel.send(new Discord.RichEmbed()
+            .setDescription('Copycat **ativado**.')
+            .setColor('#00FF00'));
+
+    collector.on('collect', collected_message => {
+        if (collected_message.content !== `${botconfig.prefix}${this.help.name}`) {
+            console.log(collected_message.content);
+            // message.channel.send(collected_message.content);
+            collector.stop('restart');
+        } else {
+            collector.stop('finished');
+        }
+    })
+
+    collector.on('end', (msg, reason) => {
+        switch (reason) {
+            case 'restart':
+                col_recursive(message, false, new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id));
+                break;
+            case 'finished':
+                return message.channel.send(new Discord.RichEmbed()
+                    .setDescription('Copycat **desativado**.')
+                    .setColor('#FF0000'));
+            default:
+                break;
+        }
+    })
+}
+
 module.exports.run = async (bot, message, args) => {
-    return message.channel.send('Este comando não está disponível no momento, tente mais tarde.');
-    // if (!collector)
-    //     message.channel.send(new Discord.RichEmbed()
-    //         .setDescription('Copycat **ativado**.')
-    //         .setColor('#00FF00'));
-    // 
-    // const collector = new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id);
-    // 
-    // collector.on('collect', copy_msg => {
-    //     if (copy_msg.content === `${botconfig.prefix}${this.help.name}`) {
-    //         collector.stop();
-    //     } else {
-    //         message.channel.send(copy_msg.content);
-    //         return collector.collected.deleteAll();
-    //     }
-    // });
-    // 
-    // collector.on('end', () => {
-    //     return message.channel.send(new Discord.RichEmbed()
-    //         .setDescription('Copycat **desativado**.')
-    //         .setColor('#FF0000'));
-    // })
+    message.channel.send('Comando indisponível no momento.');
+    // col_recursive(message, true, new Discord.MessageCollector(message.channel, m => m.author.id === message.author.id));
 }
 
 module.exports.help = {
