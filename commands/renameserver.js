d74417144d31ce73184ef50206916bf5f2618e40
Fobenga <pedro.h.s.s09@hotmail.com>
2018-12-02 01:35:55 -0300

    padronized output

--- commands/renameserver.js
+++ commands/renameserver.js
@@ -2,37 +2,33 @@ const Discord = require("discord.js");
 const botconfig = require("../botconfig.json");
 
 module.exports.run = async (bot, message, args) => {
-    const rename_embed = new Discord.RichEmbed()
-        .setAuthor(`${bot.user.username} Renomear servidor`)
-        .setFooter(`Requisitado por ${message.author.username}`, message.author.displayAvatarURL);
-
     if (message.guild.member(message.author).hasPermission('MANAGE_GUILD')) {
         var newname = args.join(" ");
 
         if (!newname || newname.length < 2 || newname.length > 100) {
-            return message.channel.send(rename_embed
+            return message.channel.send(new Discord.RichEmbed()
                 .setTitle("Uso incorreto do comando")
                 .setColor("#FF0000")
                 .addField("O nome do servidor deve ter entre 2 a 100 caracteres.",
-                    "**Tente usar: **``" + `${botconfig.prefix}${this.help.name} [novo nome]` +
-                    "``**\nOu usar: **``" + `${botconfig.prefix}help ${this.help.name}` +
-                    "``**\nPara informação detalhada sobre o comando.**"));
+                    "**Tente usar: **``" + `${botconfig.prefix}${this.help.name} [novo nome]`));
         }
 
         try {
             message.guild.setName(newname);
         } catch (e) {
-            return message.channel.send(rename_embed
-                .setTitle("Ocorreu um erro ao renomear o servidor.")
+            return message.channel.send(new Discord.RichEmbed()
+                .setDescription("Ocorreu um erro ao renomear o servidor.")
                 .setColor("#FF0000"));
         }
 
-        return message.channel.send(rename_embed
-            .setTitle(`Nome do servidor alterado de [${message.guild.name}] para [${newname}]`)
-            .setColor("#00FF00"));
+        return message.channel.send(new Discord.RichEmbed()
+            .setTitle('Nome alterado com sucesso.')
+            .setDescription(`Nome do servidor alterado de **${message.guild.name}** para **${newname}**`)
+            .setColor("#00FF00")
+            .setFooter(`Alterado por ${message.author.username}`, message.author.displayAvatarURL));
     } else {
-        return message.channel.send(rename_embed
-            .setTitle("O usuário não tem permissão para executar esse comando.")
+        return message.channel.send(new Discord.RichEmbed()
+            .setDescription("O usuário não tem permissão para executar esse comando.")
             .setColor("#FF0000"));
     }
 }
