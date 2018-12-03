ea63078a494065e92a394d9e21e12b4157e80a43
Fobenga <pedro.h.s.s09@hotmail.com>
2018-12-03 02:02:56 -0300

    fixed help

--- commands/music.js
+++ commands/music.js
@@ -492,7 +492,7 @@ async function subcmd(bot, message, args, serverQueue, user_url) {
 
 							if (start > end) return message.channel.send(new Discord.RichEmbed()
 								.setTitle('O valor **final** deve ser maior que o valor **inicial**.')
-								.setDescription("Dessa vez, tente usar ``" + `${botconfig.prefix}${module.exports.help.name} q del ${start} ${end}` + "``")
+								.setDescription("Dessa vez, tente usar ``" + `${botconfig.prefix}${module.exports.help.name} q del ${end} ${start}` + "``")
 								.setColor('#FF0000'));
 
 							if (amount < 0) amount *= -1;
