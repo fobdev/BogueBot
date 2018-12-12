0f7c7bf73e8d589c746e5c1dc5bb5b0777c141c3
Fobenga <pedro.h.s.s09@hotmail.com>
2018-12-12 17:06:10 -0300

    simple repeat-once command implemented

---
+++ commands/music/subcommands/repeat.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js')
+
+module.exports.run = async (bot, message, args, serverQueue) => {
+    if (!serverQueue) return message.channel.send(new Discord.RichEmbed()
+        .setDescription('Não tem nada tocando no momento.')
+        .setColor('$FF0000'));
+
+    let nowplaying = serverQueue.songs[0];
+    serverQueue.songs.splice(1, 0, nowplaying);
+
+    return message.channel.send(new Discord.RichEmbed()
+        .setTitle(`:repeat: **${message.author.username}** colocou para repetir`)
+        .setDescription(`[${serverQueue.songs[0].title}](${serverQueue.songs[0].url})`)
+        .setColor('#00FF00'));
+}
+
+module.exports.help = {
+    name: 'repeat',
+    name_2: 'r'
+}
