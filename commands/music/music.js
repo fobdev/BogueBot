28dcb6a1850f403e29f06348360d25c07c9b5ea6
Fobenga <pedro.h.s.s09@hotmail.com>
2018-12-23 23:33:04 -0300

    fixed missused variable

--- commands/music/music.js
+++ commands/music/music.js
@@ -403,7 +403,6 @@ async function video_player(bot, message, video, serverQueue, voiceChannel, vide
 		if (videosarray.length !== 0) {
 			for (let i = 0; i < videosarray.length; i++) {
 				if (song_playlist[i]) {
-					playlist_length += parseInt(song_playlist[i].length);
 					await serverQueue.songs.push(song_playlist[i]);
 				}
 			}
