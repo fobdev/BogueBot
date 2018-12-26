63ba0f1886467938e4755c79f5ca832a88d12471
Fobenga <pedro.h.s.s09@hotmail.com>
2018-12-26 12:06:37 -0300

    removed embeds and replied directly

--- commands/fun/random.js
+++ commands/fun/random.js
@@ -6,9 +6,7 @@ module.exports.run = async (bot, message, args) => {
     let maximum = await parseInt(args[1]);
 
     if (!args[0])
-        return message.channel.send(new Discord.RichEmbed()
-            .setTitle(`Seu numero aleatório é **${Math.floor(Math.random() * 100 + 1)}**`)
-            .setColor('#00FF00'));
+        return message.reply(`pegou o numero **${Math.floor(Math.random() * 100 + 1)}**`);
 
     if (minimum < 1 || !minimum)
         return message.channel.send(new Discord.RichEmbed()
@@ -20,13 +18,9 @@ module.exports.run = async (bot, message, args) => {
             .setColor('#FF0000'));
 
     if (!maximum)
-        return message.channel.send(new Discord.RichEmbed()
-            .setTitle(`Seu numero aleatório é **${Math.floor(Math.random() * minimum + 1)}**`)
-            .setColor('#00FF00'));
+        return message.reply(`pegou o numero **${Math.floor(Math.random() * minimum + 1)}**`);
     else
-        return message.channel.send(new Discord.RichEmbed()
-            .setTitle(`Seu numero aleatório é **${Math.floor(Math.random() * (maximum - minimum + 1)) + minimum}**`)
-            .setColor('#00FF00'));
+        return message.reply(`pegou o numero **${Math.floor(Math.random() * (maximum - minimum + 1)) + minimum}**`);
 }
 
 module.exports.help = {
