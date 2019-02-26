e068ccd7ad379a36569cd9620a6759082405c5f0
Fobenga <pedro.h.s.s09@hotmail.com>
2019-02-26 14:51:37 -0300

    unused variables removed

--- commands/bot/uptime.js
+++ commands/bot/uptime.js
@@ -1,9 +1,7 @@
 const Discord = require('discord.js');
 
 function ms_convert(ms, bot) {
-  days = Math.floor(ms / (24 * 60 * 60 * 1000));
-  daysms = ms % (24 * 60 * 60 * 1000);
-  hours = Math.floor((daysms) / (60 * 60 * 1000));
+  hours = Math.floor((ms % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
   hoursms = ms % (60 * 60 * 1000);
   minutes = Math.floor((hoursms) / (60 * 1000));
   minutesms = ms % (60 * 1000);
