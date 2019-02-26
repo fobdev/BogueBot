6cd0cd946962b924ed198a65d8dff155db03130a
Fobenga <pedro.h.s.s09@hotmail.com>
2019-02-26 15:00:36 -0300

    grater code

--- commands/bot/uptime.js
+++ commands/bot/uptime.js
@@ -8,19 +8,18 @@ function ms_convert(ms, bot) {
   minutesms = ms % (fullday / (24 * 60));
 
   let hourword = 'hora';
+  if (hours !== 1) hourword += 's';
   let minuteword = 'minuto';
-
-  if (hours !== 1) hourword = 'horas';
-  if (minutes !== 1) minuteword = 'minutos';
+  if (minutes !== 1) minuteword += 's';
 
   let hourstring = `${hours} ${hourword} e`;
   let minutestring = ` ${minutes} ${minuteword}.`;
-  let fulltime_string = ``;
 
-  if ((fullday - bot.uptime) >= (fullday / 24))
-    fulltime_string += hourstring + minutestring;
+  let fulltime_string = ``;
+  if ((fullday - bot.uptime) >= (fullday / 24)) // remaining time greater than 1 hour
+    fulltime_string += hourstring + minutestring; // include hour string
   else
-    fulltime_string += minutestring;
+    fulltime_string += minutestring; // do not include hour string
 
   return `**${fulltime_string}**`;
 }
