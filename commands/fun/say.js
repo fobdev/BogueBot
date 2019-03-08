8f8355a73249e99626f0796d3eb7afdeccd4b7c2
Fobenga <pedro.h.s.s09@hotmail.com>
2019-03-08 15:22:07 -0300

    fix administrative exploit in command

--- commands/fun/say.js
+++ commands/fun/say.js
@@ -4,6 +4,14 @@ const botconfig = require.main.require('./botconfig.json');
 module.exports.run = async (bot, message, args) => {
     let output = args.join(' ');
 
+    if (output.includes('>')) {
+        return message.channel.send(new Discord.RichEmbed()
+            .setTitle('Uso inválido do comando')
+            .setDescription("O caracter de prefixo do bot (``" + botconfig.prefix + "``) não é permitido neste comando.\n\n" +
+                "Use novamente: ``" + `${botconfig.prefix}${this.help.name} [${this.help.arg}]` + "``")
+            .setColor('#FF0000'));
+    }
+
     if (!output)
         return message.channel.send(new Discord.RichEmbed()
             .setTitle('Uso incorreto do comando')
