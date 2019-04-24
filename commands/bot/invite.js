2a17a0992941c909b69aab7ede0607e07feabe77
Fobenga <pedro.h.s.s09@hotmail.com>
2019-04-24 16:27:26 -0300

    new log

--- commands/bot/invite.js
+++ commands/bot/invite.js
@@ -3,12 +3,20 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     var current_servers = bot.guilds.array();
 
-    console.log("---------------------------------");
-    console.log(`Currently connected to [${current_servers.length}] servers.\nServer List:`);
-    for (var i = 0; i < current_servers.length; i++) {
-        console.log(`${i + 1} - [${current_servers[i]}]`);
+    let members_reached = 0;
+    console.log("--------------------------------------------");
+    console.log(`Connected to [${current_servers.length}] servers.\nServer List:`);
+
+    // Get the name of all the servers
+    for (let i = 0; i < current_servers.length; i++) {
+        let leftzero = '';
+        if (i < 9) leftzero += '0';
+        members_reached += current_servers[i].memberCount;
+        console.log(`${leftzero}${i + 1} - [${current_servers[i]}] [${current_servers[i].memberCount} members]`);
     }
-    console.log("---------------------------------");
+    console.log("--------------------------------------------");
+    console.log(`A total of [${members_reached}] Discord users reached.`);
+    console.log("--------------------------------------------");
 
     return bot.generateInvite(8)
         .then(link => message.channel.send(new Discord.RichEmbed()
