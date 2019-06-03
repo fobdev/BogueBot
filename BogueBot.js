e3eaa922b901b9894a6b6c8ab57d6f27c8d23abd
Fobenga <pedro.h.s.s09@hotmail.com>
2019-06-03 15:38:24 -0300

    fixed permission errors

--- BogueBot.js
+++ BogueBot.js
@@ -157,15 +157,17 @@ bot.on('guildDelete', guild => {
 });
 
 bot.on('guildMemberRemove', member => {
+    // get the current guild that the bot is in.
+    console.log(`[${member.user.username}] left [${member.guild.name}]`);
+
     const system_channel = member.guild.channels.find(ch => ch.id === member.guild.systemChannelID);
     if (system_channel) {
-        try {
+        if (member.guild.member(bot.user).hasPermission('ADMINISTRATOR')) {
             system_channel.send(`**${member}** kito do sv...`);
             console.log(`[MEMBER LEAVE] member [${member.user.username}] leave message successfully sent to ${member.guild.name} system channel.`);
-        } catch (e) {
+        } else {
             console.log(`[MEMBER LEAVE] member [${member.user.username}] left but bot has no permissions to send messages to system channel.`);
         }
-
     }
 })
 
