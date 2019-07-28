ffad10390da91caea0f45eec9ecc5fb8d8c96c1d
Pedro Henrique <silvapedro55@gmail.com>
2019-07-28 16:50:52 -0300

    changed output for no authorized users

--- commands/admin/serverlist.js
+++ commands/admin/serverlist.js
@@ -1,26 +1,35 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    if (message.author.id === '244270921286811648') {
-        let current_servers = bot.guilds.array();
+  if (message.author.id === "244270921286811648") {
+    let current_servers = bot.guilds.array();
 
-        let guildstring = "";
-        let membercount = 0;
-        for (let i = 0; i < current_servers.length; i++) {
-            let leftzero = '';
-            if (i < 9) leftzero += '0';
-            membercount += current_servers[i].memberCount;
-            guildstring += `${leftzero}${i + 1} - [${current_servers[i]}] [${current_servers[i].memberCount} membros]\n`;
-        }
-
-        return message.channel.send("```" + guildstring + `\nUm total de [${membercount}] usuários alcançados` + "```");
-    } else {
-        return message.channel.send(new Discord.RichEmbed()
-            .setDescription("Apenas **Fobenga** pode usar esse comando.")
-            .setColor('#FF0000'));
+    let guildstring = "";
+    let membercount = 0;
+    for (let i = 0; i < current_servers.length; i++) {
+      let leftzero = "";
+      if (i < 9) leftzero += "0";
+      membercount += current_servers[i].memberCount;
+      guildstring += `${leftzero}${i + 1} - [${current_servers[i]}] [${
+        current_servers[i].memberCount
+      } membros]\n`;
     }
-}
+
+    return message.channel.send(
+      "```" +
+        guildstring +
+        `\nUm total de [${membercount}] usuários alcançados` +
+        "```"
+    );
+  } else {
+    return message.channel.send(
+      new Discord.RichEmbed()
+        .setDescription("Só **Fobenga** pode usar esse comando mals.")
+        .setColor("#FF0000")
+    );
+  }
+};
 
 module.exports.help = {
-    name: 'serverlist'
-}
+  name: "serverlist"
+};
