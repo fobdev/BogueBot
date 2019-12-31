5fbcda16350ca386fee0101c83b3eb7e1e99381a
Pedro Henrique <silvapedro55@gmail.com>
2019-12-30 22:08:28 -0300

    image file now deleted after sending

--- commands/user/couple.js
+++ commands/user/couple.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 const botconfig = require.main.require("./botconfig.json");
 const mergeImg = require('merge-img');
+const fs = require('fs');
 
-let global_count = 0;
 module.exports.run = async (bot, message, args) => {
     let user1 = message.guild.member(message.mentions.users.firstKey());
     let user2 = message.guild.member(message.mentions.users.lastKey());
-    if (!args[0] || (!args[0] && !args[1]) || !args[1]) {
+    if (!args[0] || (!args[0] && !args[1]) || !args[1] || args.length > 2) {
         return message.channel.send(new Discord.RichEmbed()
             .setTitle('Uso incorreto do comando.')
             .setDescription("Tente usar: ``" + botconfig.prefix + this.help.name + ` [${this.help.arg.join('][')}]` + "``")
@@ -14,21 +14,38 @@ module.exports.run = async (bot, message, args) => {
     }
 
     let firstmention_id = args[0].slice(0, -1).replace(/[\\<>@#&!]/g, "");
+    let filename = `couple_${message.guild.id}_${message.id}.png`;
     if (firstmention_id != user1.user.id) {
         await mergeImg([user2.user.displayAvatarURL + "?size=8192", user1.user.displayAvatarURL + "?size=8192"]).then((img) => {
-            img.write(`couple_${message.guild.id}_${global_count}.png`, () => {
-                return message.channel.send(new Discord.Attachment(`couple_${message.guild.id}_${global_count}.png`))
+            console.log(`Couple image generated sucessfully as [${filename}] locally.`);
+            img.write(filename, () => {
+                // send
+                message.channel.send(new Discord.Attachment(filename))
+                // delete
+                try {
+                    fs.unlinkSync(`./${filename}`);
+                    console.log(`File [${filename}] deleted sucessfully after sending.`)
+                } catch (e) {
+                    console.log(`Error in deleting file ${filename}: ${e}`)
+                }
             })
         });
     } else {
         await mergeImg([user1.user.displayAvatarURL + "?size=8192", user2.user.displayAvatarURL + "?size=8192"]).then((img) => {
-            img.write(`couple_${message.guild.id}_${global_count}.png`, () => {
-                return message.channel.send(new Discord.Attachment(`couple_${message.guild.id}_${global_count}.png`))
+            console.log(`Couple image generated sucessfully as [${filename}] locally.`);
+            img.write(filename, () => {
+                // send
+                message.channel.send(new Discord.Attachment(filename))
+                // delete
+                try {
+                    fs.unlinkSync(`./${filename}`);
+                    console.log(`File [${filename}] deleted sucessfully after sending.`)
+                } catch (e) {
+                    console.log(`Error in deleting file ${filename}: ${e}`)
+                }
             })
         });
     }
-
-    console.log(`Couple image generated sucessfully as [couple_${message.guild.id}_${global_count}.png] locally.`);
 }
 
 module.exports.help = {
