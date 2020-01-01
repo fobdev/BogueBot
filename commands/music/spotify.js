f87d698440d4a21cf9d9c414b9cac6ebb09a8437
Pedro Henrique <silvapedro55@gmail.com>
2020-01-01 19:02:59 -0300

    removed variable that was causing problems

--- commands/music/spotify.js
+++ commands/music/spotify.js
@@ -2,21 +2,20 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let user = message.mentions.users.first() || message.author;
-    let upr = user.presence.activity;
     if (user.presence.activity !== null &&
-        upr.type === 'LISTENING' &&
-        upr.name === 'Spotify' &&
-        upr.assets !== null) {
-        let trackImgURL = `https://i.scdn.co/image/${upr.assets.largeImage.slice(8)}`;
-        let trackUrl = `https://open.spotify.com/track/${upr.syncID}`;
+        user.presence.activity.type === 'LISTENING' &&
+        user.presence.activity.name === 'Spotify' &&
+        user.presence.activity.assets !== null) {
+        let trackImgURL = `https://i.scdn.co/image/${user.presence.activity.assets.largeImage.slice(8)}`;
+        let trackUrl = `https://open.spotify.com/track/${user.presence.activity.syncID}`;
         const spotifyEmoji = 'https://cdn.discordapp.com/emojis/408668371039682560.png';
 
         return message.channel.send(new Discord.RichEmbed()
             .setAuthor(`Música que ${user.username} está ouvindo no Spotify.`, spotifyEmoji)
             .setThumbnail(trackImgURL)
-            .addField('Nome', upr.details, true)
-            .addField('Album', upr.assets.largeText, true)
-            .addField('Autor', upr.state, true)
+            .addField('Nome', user.presence.activity.details, true)
+            .addField('Album', user.presence.activity.assets.largeText, true)
+            .addField('Autor', user.presence.activity.state, true)
             .addField('Ouvir a música: ', `[\`${trackUrl}\`](trackUrl)`, false)
             .setColor(0x1ED760));
     } else {
