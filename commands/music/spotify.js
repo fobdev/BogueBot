6590c8bb2f0ff9a75a7577524c3bf311cd06983a
Pedro Henrique <silvapedro55@gmail.com>
2020-01-01 19:07:32 -0300

    typo checked in docs corrected

--- commands/music/spotify.js
+++ commands/music/spotify.js
@@ -2,20 +2,22 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let user = message.mentions.users.first() || message.author;
-    if (user.presence.activity !== null &&
-        user.presence.activity.type === 'LISTENING' &&
-        user.presence.activity.name === 'Spotify' &&
-        user.presence.activity.assets !== null) {
-        let trackImgURL = `https://i.scdn.co/image/${user.presence.activity.assets.largeImage.slice(8)}`;
-        let trackUrl = `https://open.spotify.com/track/${user.presence.activity.syncID}`;
+    let user = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+
+    if (user.presence.game !== null &&
+        user.presence.game.type === 'LISTENING' &&
+        user.presence.game.name === 'Spotify' &&
+        user.presence.game.assets !== null) {
+        let trackImgURL = `https://i.scdn.co/image/${user.presence.game.assets.largeImage.slice(8)}`;
+        let trackUrl = `https://open.spotify.com/track/${user.presence.game.syncID}`;
         const spotifyEmoji = 'https://cdn.discordapp.com/emojis/408668371039682560.png';
 
         return message.channel.send(new Discord.RichEmbed()
             .setAuthor(`Música que ${user.username} está ouvindo no Spotify.`, spotifyEmoji)
             .setThumbnail(trackImgURL)
-            .addField('Nome', user.presence.activity.details, true)
-            .addField('Album', user.presence.activity.assets.largeText, true)
-            .addField('Autor', user.presence.activity.state, true)
+            .addField('Nome', user.presence.game.details, true)
+            .addField('Album', user.presence.game.assets.largeText, true)
+            .addField('Autor', user.presence.game.state, true)
             .addField('Ouvir a música: ', `[\`${trackUrl}\`](trackUrl)`, false)
             .setColor(0x1ED760));
     } else {
