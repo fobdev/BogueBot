90d0cdd47166f2cd86862849d9438cc534b1a0bd
Pedro Henrique <silvapedro55@gmail.com>
2020-02-25 21:09:08 -0300

    fixed dir

--- commands/fun/boleta.js
+++ commands/fun/boleta.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-    let ze = new Discord.Attachment('../../boleta.jpeg');
+    let ze = new Discord.Attachment('../boleta.jpeg');
     await message.channel.send(new Discord.RichEmbed()
         .setTitle("Parabéns Zé.")
         .attachFile(ze)
