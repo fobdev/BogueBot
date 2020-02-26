25ad80dbce883d2c624f9cf2c062b2c1fe763a3e
Pedro Henrique <silvapedro55@gmail.com>
2020-02-25 21:03:54 -0300

    fixed issue with localization and added title

--- commands/fun/boleta.js
+++ commands/fun/boleta.js
@@ -1,10 +1,12 @@
 const Discord = require('discord.js');
+const boletafile = require.main.require("./boleta.jpeg");
 
 module.exports.run = async (bot, message, args) => {
     let ze = new Discord.Attachment('../../boleta.jpeg');
     await message.channel.send(new Discord.RichEmbed()
-        .attachFile(ze)
-        .setImage(`attachment://${'bog.png'}`)
+        .setTitle("Parabéns Zé.")
+        .attachFile(boletafile)
+        .setImage(`attachment://${'boleta.jpeg'}`)
         .setColor("#00FF00"));
 }
 
