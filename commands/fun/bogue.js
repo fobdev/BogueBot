d2238a92110344e6f2fc3f394274de488af49112
Pedro Henrique <silvapedro55@gmail.com>
2020-03-23 22:41:24 -0300

    commands moved to a primary command

--- commands/fun/bogue.js
+++ commands/fun/bogue.js
@@ -1,19 +1,42 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-    if (args[0] == 'prime') {
-        let filename = 'bogueprime.jpg';
-        let attachment = new Discord.Attachment(filename);
 
-        await message.channel.send(new Discord.RichEmbed()
-            .setTitle("Bogue Prime")
-            .attachFile(attachment)
-            .setImage(`attachment://${filename}`)
-            .setColor("#00FF00"));
+    switch (args[0]) {
+        case 'prime':
+            let filename = 'bogueprime.jpg';
+            let attachment = new Discord.Attachment(filename);
+
+            await message.channel.send(new Discord.RichEmbed()
+                .setTitle("Bogue Prime")
+                .attachFile(attachment)
+                .setImage(`attachment://${filename}`)
+                .setColor("#00FF00"));
+            break;
+        case 'pixel':
+            let filename = 'boguepixel.jpg';
+            let attachment = new Discord.Attachment(filename);
+
+            await message.channel.send(new Discord.RichEmbed()
+                .setTitle("Bogue Pixel")
+                .attachFile(attachment)
+                .setImage(`attachment://${filename}`)
+                .setColor("#00FF00"));
+            break;
+        default:
+            let filename = 'bogue.jpg';
+            let attachment = new Discord.Attachment(filename);
+
+            await message.channel.send(new Discord.RichEmbed()
+                .setTitle("Bogue")
+                .attachFile(attachment)
+                .setImage(`attachment://${filename}`)
+                .setColor("#00FF00"));
+            break;
     }
 }
 
 module.exports.help = {
     name: 'bogue',
-    descr: `Bogue Prime`
+    descr: `Formas diversas de bogue.`
 }
