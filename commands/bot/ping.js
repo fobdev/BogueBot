08139de715a9e2f97b6c395ac41364f1ce440fe2
Pedro Henrique <silvapedro55@gmail.com>
2020-04-04 14:43:31 -0300

    checking for autodeploy

--- commands/bot/ping.js
+++ commands/bot/ping.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
 	var ping = await bot.ping;
+	message.channel.send('autodeploy test sucess.');
 	return message.channel.send(new Discord.RichEmbed()
 		.setDescription(`**${bot.user.username} Ping: ${Math.floor(ping)}ms**`)
 		.setColor("#00FF00"));
