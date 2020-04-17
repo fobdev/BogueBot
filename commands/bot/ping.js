69f2c7524aeb408481dfe01e85e1b4fc418898ca
Pedro Henrique <silvapedro55@gmail.com>
2020-04-17 10:44:01 -0300

    removed autodeploy test message

--- commands/bot/ping.js
+++ commands/bot/ping.js
@@ -2,7 +2,6 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
 	var ping = await bot.ping;
-	message.channel.send('autodeploy test sucess.');
 	return message.channel.send(new Discord.RichEmbed()
 		.setDescription(`**${bot.user.username} Ping: ${Math.floor(ping)}ms**`)
 		.setColor("#00FF00"));
