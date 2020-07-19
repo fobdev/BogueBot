7f31428e0c60400ef9075d492cac05a147fbaf7a
Pedro Henrique <silvapedro55@gmail.com>
2020-07-19 00:24:13 -0300

    now gifs move!

--- commands/user/avatar.js
+++ commands/user/avatar.js
@@ -3,23 +3,38 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 	let user_avatar = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
 	let sv_icon = args.join(" ");
-	const avatar_embed = new Discord.RichEmbed()
-		.setFooter(`Requisitado por ${message.author.username}`, message.author.displayAvatarURL);
 
 	if (!user_avatar) {
 		if (sv_icon === 'server') {
-			return message.channel.send(avatar_embed.setTitle(`Ícone do servidor ${message.guild.name}`)
-				.setImage(message.guild.iconURL)
+			if (message.guild.iconURL.endsWith(".gif")) {
+				// Do not generate a rich embed, instaed just send the message.
+				return message.channel.send(message.guild.iconURL);
+			} else {
+				return message.channel.send(new Discord.RichEmbed()
+					.setTitle(`Ícone do servidor ${message.guild.name}`)
+					.setImage(message.guild.iconURL)
+					.setColor("#00FF00"));
+			}
+		}
+
+		if (message.author.displayAvatarURL.endsWith(".gif")) {
+			return message.channel.send(message.author.displayAvatarURL);
+		} else {
+			return message.channel.send(new Discord.RichEmbed()
+				.setTitle(`Avatar de **${message.author.username}**`)
+				.setImage(message.author.displayAvatarURL)
 				.setColor("#00FF00"));
 		}
-		return message.channel.send(avatar_embed.setTitle(`Avatar de **${message.author.username}**`)
-			.setImage(message.author.displayAvatarURL)
-			.setColor("#00FF00"));
 	}
 
-	return message.channel.send(avatar_embed.setTitle(`Avatar de **${user_avatar.user.username}**`)
-		.setImage(user_avatar.user.displayAvatarURL)
-		.setColor("#00FF00"));
+	if (user_avatar.user.displayAvatarURL.endsWith(".gif")) {
+		return message.channel.send(user_avatar.user.displayAvatarURL);
+	} else {
+		return message.channel.send(new Discord.RichEmbed()
+			.setTitle(`Avatar de **${user_avatar.user.username}**`)
+			.setImage(user_avatar.user.displayAvatarURL)
+			.setColor("#00FF00"));
+	}
 }
 
 module.exports.help = {
