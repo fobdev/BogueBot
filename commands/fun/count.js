9bea895d149c814847620056e0e111ed888668fd
Pedro Henrique <silvapedro55@gmail.com>
2020-10-01 00:23:34 -0300

    contador de risadas

---
+++ commands/fun/count.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    if(message.content.includes('KKK') || message.content.includes('kkk') ){
+        let msgarray = message.content;
+        let kcount = 0;
+        
+        for (let int = 0; int < msgarray.length; int++) 
+            if(msgarray[i] == 'K' || msgarray[i] == 'k')
+                kcount++;
+
+        return message.channel.send(`${kcount} Ks`);
+    } 
+    else 
+        message.channel.send("Não vi nenhuma risada na mensagem acima.");
+}
+
+module.exports.help = {
+    name: 'contar',
+    descr: `Conta as risadas da mensagem acima`
+}
