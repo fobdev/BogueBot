42f4ef57d42acac95a762399e893caf9d3495aa9
Pedro Henrique <silvapedro55@gmail.com>
2020-12-28 20:49:33 -0300

    removed problematic logging

--- commands/user/avatar.js
+++ commands/user/avatar.js
@@ -1,5 +1,4 @@
 const Discord = require("discord.js");
-const validurl = require("valid-url");
 const https = require("https");
 
 module.exports.run = async (bot, message, args) => {
@@ -46,10 +45,8 @@ function userAvatar(validURL, uAvatar) {
 		.setTitle(`Avatar de **${uAvatar.user.username}**`)
 		.setColor("#00FF00");
 	if (validURL) {
-		console.log(`VALIDATED! url: ${gifUserURL}`)
 		return message.channel.send(userEmbed.setImage(gifUserURL))
 	} else {
-		console.log(`NOT VALIDATED! url: ${gifGuildURL}\nSHOULD USE ${uAvatar.user.displayAvatarURL()}`)
 		return message.channel.send(userEmbed.setImage(uAvatar.user.displayAvatarURL()))
 	}
 }
@@ -60,10 +57,8 @@ function guildAvatar(validURL) {
 		.setColor("#00FF00");
 
 	if (validURL) {
-		console.log(`VALIDATED! url: ${gifGuildURL} [isURLValid: ${isURLValid}]`)
 		return message.channel.send(serverEmbed.setImage(gifGuildURL));
 	} else {
-		console.log(`NOT VALIDATED! url: ${gifGuildURL} [isURLValid: ${isURLValid}]\nSHOULD USE: ${message.guild.iconURL()}`)
 		return message.channel.send(serverEmbed.setImage(message.guild.iconURL()));
 	}
 }
@@ -73,10 +68,8 @@ function authorAvatar(validURL) {
 		.setTitle(`Avatar de **${message.author.username}**`)
 		.setColor("#00FF00");
 	if (validURL) {
-		console.log(`VALIDATED! url: ${gifAuthorURL}`)
 		return message.channel.send(authorEmbed.setImage(gifAuthorURL));
 	} else {
-		console.log(`NOT VALIDATED! url: ${gifAuthorURL}\nSHOULD USE: ${message.author.displayAvatarURL()}`)
 		return message.channel.send(authorEmbed.setImage(message.author.displayAvatarURL()));
 	}
 }
