374369063c4419788671814f6017cba559b5a811
Pedro Henrique <silvapedro55@gmail.com>
2020-12-28 18:18:14 -0300

    fix avatar usage using trycatch

--- commands/user/avatar.js
+++ commands/user/avatar.js
@@ -4,50 +4,59 @@ module.exports.run = async (bot, message, args) => {
 	let user_avatar = message.guild.member(message.mentions.users.first());
 	let argument = args.join(" ");
 
+
+	// in the case of no mentions
 	if (!user_avatar) {
 		if (argument === 'server')
-			return message.channel.send(new Discord.MessageEmbed()
-				.setTitle(`Ícone do servidor ${message.guild.name}`)
-				.setImage(message.guild.iconURL())
-				.setColor("#00FF00"));
+			try {
+				return message.channel.send(new Discord.MessageEmbed()
+					.setTitle(`Ícone do servidor ${message.guild.name}`)
+					.setImage(message.guild.iconURL({
+						format: 'gif'
+					}))
+					.setColor("#00FF00"));
+			} catch (e) {
+				console.log("Image was not a gif, using original.");
+				return message.channel.send(new Discord.MessageEmbed()
+					.setTitle(`Ícone do servidor ${message.guild.name}`)
+					.setImage(message.guild.iconURL())
+					.setColor("#00FF00"));
+			}
 
-		console.log(`original 1: ${message.author.displayAvatarURL()}`);
-
-		if (message.author.displayAvatarURL().endsWith(".gif")) {
-			let attachPic = message.author.displayAvatarURL({
-				format: "gif"
-			});
-			console.log(`original 2: ${message.author.displayAvatarURL()}`);
-			message.channel.send(new Discord.MessageEmbed()
-				.setTitle(`Avatar de **${message.author.username}**`)
-				.attachFiles([new Discord.MessageAttachment(attachPic)])
-				.setImage(`attachment://${attachPic}`)
-				.setColor("#00FF00"));
-			console.log(`after sending: ${message.author.displayAvatarURL()}`);
-		} else {
+	} else {
+		try {
 			return message.channel.send(new Discord.MessageEmbed()
 				.setTitle(`Avatar de **${message.author.username}**`)
 				.setImage(message.author.displayAvatarURL())
 				.setColor("#00FF00"));
+		} catch (e) {
+			console.log("Image was not a gif, using original.");
+			return message.channel.send(new Discord.MessageEmbed()
+				.setTitle(`Avatar de **${message.author.username}**`)
+				.setImage(message.author.displayAvatarURL({
+					format: "gif"
+				}))
+				.setColor("#00FF00"));
 		}
 	}
 
-	if (user_avatar.user.displayAvatarURL().endsWith(".gif")) {
-		let attachPic = user_avatar.user.displayAvatarURL({
-			format: "gif"
-		});
+	// in the case of mentions
+	try {
 		return message.channel.send(new Discord.MessageEmbed()
 			.setTitle(`Avatar de **${user_avatar.user.username}**`)
-			.attachFiles([new Discord.MessageAttachment(attachPic)])
-			.setImage(`attachment://${attachPic}`)
+			.setImage(user_avatar.user.displayAvatarURL({
+				format: "gif"
+			}))
 			.setColor("#00FF00"));
-	} else
+	} catch (e) {
+		console.log("Image was not a gif, using original.");
 		return message.channel.send(new Discord.MessageEmbed()
 			.setTitle(`Avatar de **${user_avatar.user.username}**`)
 			.setImage(user_avatar.user.displayAvatarURL())
 			.setColor("#00FF00"));
-}
+	}
 
+}
 module.exports.help = {
 	name: "avatar",
 	descr: 'Mostra o avatar de um usuário.',
