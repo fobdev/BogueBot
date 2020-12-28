7991248e1322b7c4997295516ba1e87112ceca49
Pedro Henrique <silvapedro55@gmail.com>
2020-12-28 19:48:43 -0300

    completely different implementation

--- commands/user/avatar.js
+++ commands/user/avatar.js
@@ -5,51 +5,65 @@ const https = require("https");
 module.exports.run = async (bot, message, args) => {
 	let user_avatar = message.guild.member(message.mentions.users.first());
 	let argument = args.join(" ");
+	let isURLValid = true;
+	let gifGuildURL = message.guild.iconURL({
+		format: 'gif'
+	});
+	let gifAuthorURL = message.author.displayAvatarURL({
+		format: 'gif'
+	});
+	let gifUserURL = user_avatar.user.displayAvatarURL({
+		format: 'gif'
+	});
+
+	https.get((gifGuildURL), (res) => {
+		if (res.statusCode != 200)
+			isURLValid = false;
+	})
+
+	https.get((gifAuthorURL), (res) => {
+		if (res.statusCode != 200)
+			isURLValid = false;
+	})
+
+	https.get((gifUserURL), (res) => {
+		if (res.statusCode != 200)
+			isURLValid = false;
+	})
 
 	// in the case of no mentions
 	if (!user_avatar) {
 		if (argument === 'server')
-			return message.channel.send(new Discord.MessageEmbed()
-				.setTitle(`Ícone do servidor ${message.guild.name}`)
-				.setImage(message.guild.iconURL({
-					format: 'gif'
-				}))
-				.setColor("#00FF00"));
-		else
-			return message.channel.send(new Discord.MessageEmbed()
+			if (isURLValid) {
+				let serverEmbed = new Discord.MessageEmbed()
+					.setTitle(`Ícone do servidor ${message.guild.name}`)
+					.setColor("#00FF00");
+
+				return message.channel.send(serverEmbed.setImage(gifGuildURL));
+			} else
+				return message.channel.send(serverEmbed.setImage(message.guild.iconURL()));
+		else {
+			let authorEmbed = new Discord.MessageEmbed()
 				.setTitle(`Avatar de **${message.author.username}**`)
-				.setImage(message.author.displayAvatarURL({
-					format: 'gif'
-				}))
-				.setColor("#00FF00"));
+				.setColor("#00FF00");
+
+			if (isURLValid)
+				message.channel.send(authorEmbed.setImage(gifAuthorURL));
+			else
+				message.channel.send(authorEmbed.setImage(message.author.displayAvatarURL()));
+		}
 	}
 
-	https.get(user_avatar.user.displayAvatarURL({
-		format: 'gif'
-	}), (res) => {
-		console.log(`statusCode from user GIF: ${res.statusCode}`)
-	})
+	let userEmbed = new Discord.MessageEmbed()
+		.setTitle(`Avatar de **${user_avatar.user.username}**`)
+		.setColor("#00FF00");
 
-	https.get(user_avatar.user.displayAvatarURL(), (res) => {
-		console.log(`statusCode from default: ${res.statusCode}`)
-	})
-
-	if (validurl.isUri(uavt)) {
-		console.log(`validated! ${uavt}`);
-		return message.channel.send(new Discord.MessageEmbed()
-			.setTitle(`Avatar de **${user_avatar.user.username}**`)
-			.setImage(user_avatar.user.displayAvatarURL({
-				format: 'gif'
-			}))
-			.setColor('#00FF00'))
-	} else {
-		console.log(`not validated! ${uavt}`);
-		return message.channel.send(new Discord.MessageEmbed()
-			.setTitle(`Avatar de **${user_avatar.user.username}**`)
-			.setImage(user_avatar.user.displayAvatarURL())
-			.setColor('#00FF00'))
-	}
+	if (isURLValid)
+		return message.channel.send(userEmbed.setImage(gifUserURL))
+	else
+		return message.channel.send(userEmbed.setImage(user_avatar.user.displayAvatarURL()))
 }
+
 module.exports.help = {
 	name: "avatar",
 	descr: 'Mostra o avatar de um usuário.',
