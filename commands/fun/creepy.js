d121787629a7319b0d6a4394d232055d92302fbb
Pedro Henrique <silvapedro55@gmail.com>
2020-12-29 18:29:31 -0300

    log errors

--- commands/fun/creepy.js
+++ commands/fun/creepy.js
@@ -6,8 +6,10 @@ module.exports.run = async (bot, message, args) => {
     try {
         // tries to delete but pass it if doesn't have permissions
         await message.delete();
+        console.log('User message deleted.');
         return message.channel.send(lunicode.tools.creepify.encode(input));
     } catch (e) {
+        console.log(`Failed to delete user message: ${e}`);
         return message.channel.send(lunicode.tools.creepify.encode(input));
     }
 }
