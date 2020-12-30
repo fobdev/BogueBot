a3a07e410f2050ab4274b01a72ab2ecdfc81fd41
Pedro Henrique <silvapedro55@gmail.com>
2020-12-29 23:09:17 -0300

    using new algorithm

--- commands/user/avatar.js
+++ commands/user/avatar.js
@@ -6,9 +6,42 @@ module.exports.run = async (bot, message, args) => {
 
 	// in the case of no mentions
 	if (!user_avatar) {
+		if (args.join(' ') === 'server') {
+			// shows avatar from the server
+			return message.channel.send(new Discord.MessageEmbed()
+				.setTitle(`:frame_photo: Ícone do servidor ${message.guild.name}`)
+				.setImage(message.guild.iconURL({
+					dynamic: true,
+					size: 512
+				}))
+				.setColor("#FF0000"))
+		} else {
+			// shows avatar from the message sender
+			return message.channel.send(new Discord.MessageEmbed()
+				.setTitle(`:frame_photo: Avatar de ${message.author.username}`)
+				.setImage(message.author.displayAvatarURL({
+					dynamic: true,
+					size: 512
+				}))
+				.setColor('#00FF00'));
+		}
+	} else {
+		// shows avatar from the actual mentioned person
+		return message.channel.send(new Discord.MessageEmbed()
+			.setTitle(`:frame_photo: Avatar de ${user_avatar.user.username}`)
+			.setImage(user_avatar.user.displayAvatarURL({
+				dynamic: true,
+				size: 512
+			}))
+			.setColor('#00FF00'));
+	}
+
+	/*
+	if (!user_avatar) {
 		if (args.join(" ") === 'server') {
 			let gifGuildURL = message.guild.iconURL({
-				format: 'gif'
+				format: 'gif',
+				size: 512
 			});
 
 			return https.get((gifGuildURL), (res) => {
@@ -19,7 +52,8 @@ module.exports.run = async (bot, message, args) => {
 
 		} else {
 			let gifAuthorURL = message.author.displayAvatarURL({
-				format: 'gif'
+				format: 'gif',
+				size: 512
 			});
 
 			return https.get((gifAuthorURL), async (res) => {
@@ -32,13 +66,14 @@ module.exports.run = async (bot, message, args) => {
 
 	let gifUserURL = user_avatar.user.displayAvatarURL({
 		format: 'gif',
-		size: 256
+		size: 512
 	});
 	return https.get((gifUserURL), (res) => {
 		if (res.statusCode != 200)
 			userAvatar(message, false, user_avatar, gifUserURL)
 		else userAvatar(message, true, user_avatar, gifUserURL);
 	})
+	 */
 }
 
 function userAvatar(message, validURL, uAvatar, gifUserURL) {
