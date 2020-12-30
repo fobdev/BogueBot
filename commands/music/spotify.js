6c1036af3864fb7888b3a2d4c79618afa14200d7
Pedro Henrique <silvapedro55@gmail.com>
2020-12-30 00:29:44 -0300

    trying new implementation

--- commands/music/spotify.js
+++ commands/music/spotify.js
@@ -2,27 +2,23 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     let user = message.mentions.users.first() || message.author;
-
-    console.log(message.author.presence.activities.name);
-    if (user.presence.activities.type == 'LISTENING' &&
-        user.presence.activities.name == 'Spotify') {
-        let trackImgURL = `https://i.scdn.co/image/${user.presence.game.assets.largeImage.slice(8)}`;
-        let trackUrl = `https://open.spotify.com/track/${user.presence.game.syncID}`;
+    let spotifyActivity = user.presence.activities.find(activity => activity.name = 'Spotify');
+    if (spotifyActivity) {
+        let trackImgURL = `https://i.scdn.co/image/${spotifyActivity.assets.largeImage.slice(8)}`;
         const spotifyEmoji = 'https://cdn.discordapp.com/emojis/408668371039682560.png';
 
         if (args[0] == 'art')
             return message.channel.send(new Discord.MessageEmbed()
-                .setTitle(`Arte do album ${user.presence.game.assets.largeText}, de ${user.presence.game.state}`)
+                .setTitle(`Arte do album ${spotifyActivity.assets.largeText}, de ${spotifyActivity.state}`)
                 .setImage(trackImgURL)
                 .setColor("#00FF00"));
         else
             return message.channel.send(new Discord.MessageEmbed()
                 .setAuthor(`Música que ${user.username} está ouvindo no Spotify.`, spotifyEmoji)
                 .setThumbnail(trackImgURL)
-                .addField('Nome', user.presence.game.details, true)
-                .addField('Album', user.presence.game.assets.largeText, true)
-                .addField('Autor', user.presence.game.state, true)
-                .addField('Ouvir a música: ', `[${trackUrl}](${trackUrl})`, false)
+                .addField('Nome', spotifyActivity.details, true)
+                .addField('Album', spotifyActivity.assets.largeText, true)
+                .addField('Autor', spotifyActivity.state, true)
                 .setDescription("Clique na thumbnail da capa para ve-la em alta resolução.")
                 .setColor(0x1ED760));
     } else
