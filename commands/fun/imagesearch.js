545db52d653ac3bf9ce6b2b4e7dded2edd21d36e
Pedro Henrique <silvapedro55@gmail.com>
2021-01-20 17:41:31 -0300

    error fixing and quality of life improvements

--- commands/fun/imagesearch.js
+++ commands/fun/imagesearch.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const gis = require('g-i-s');
+const fetch = require('node-fetch');
 
 module.exports.run = async (bot, message, args) => {
     let fullmsg = args.join(" ");
@@ -10,67 +11,103 @@ module.exports.run = async (bot, message, args) => {
             .setDescription(`Digite alguma imagem que queira buscar com >${this.help.name} [${this.help.arg}]`)
             .setColor('#FF0000'));
     else {
-        gis(fullmsg, (e, res) => {
-            if (e) console.log(e);
-            else {
-                let answer = new Discord.MessageEmbed()
-                    .setTitle(`Resultados para **${fullmsg}** (1/${res.length})`)
-                    .setImage(res[0].url)
-                    .setColor("#00FF00");
+        message.channel.send(new Discord.MessageEmbed()
+            .setDescription(`Buscando por **${fullmsg}**...`)
+            .setColor("#00FF00")).then(async msg => {
+            gis(fullmsg, async (e, unfiltered) => {
+                if (e) console.log(e);
+                else if (unfiltered.length < 2) {
+                    return msg.edit(new Discord.MessageEmbed()
+                        .setTitle('Erro na busca')
+                        .setDescription('Não foi encontrado nenhum resultado para sua busca.')
+                        .setColor('#FF0000'));
+                } else {
+                    let image_index = 0;
+                    let res = [];
 
-                message.channel.send(answer).then(msg => {
-                    msg.react('⬅');
-                    msg.react('➡');
+                    function errorHandler(response) {
+                        if (!response.ok)
+                            throw Error(response.statusText);
+                        return response;
+                    }
+
+                    // load only the first result
+                    for (let i = 0; i < unfiltered.length; i++) {
+                        let valid = false;
+                        await fetch(unfiltered[i].url).then(errorHandler).then(response => {
+                            if (response.status == 200) {
+                                valid = true;
+                                res.push(unfiltered[i]);
+                            }
+                        }).catch(error => null)
+                        if (valid) break;
+                    }
+
+                    // compute other results
+                    for (let i = 0; i < unfiltered.length; i++) {
+                        fetch(unfiltered[i].url).then(errorHandler).then(response => {
+                            if (response.status == 200)
+                                res.push(unfiltered[i]);
+                        }).catch(error => null)
+                    }
+
+                    await msg.edit(new Discord.MessageEmbed()
+                        .setTitle(`Resultados para **${fullmsg}**`)
+                        .setImage(res[image_index].url)
+                        .setColor("#00FF00"));
+
+                    if (image_index != 0)
+                        msg.react('⬅');
+
+                    if (image_index != res.length)
+                        msg.react('➡');
 
                     let reactions = msg.createReactionCollector((reaction, user) => (reaction.emoji.name === '➡' || (reaction.emoji.name === '⬅')) && user.id != bot.user.id, {
                         time: 5 * 60 * 1000 // 5 min cd
                     });
 
-                    let image_index = 0;
                     reactions.on('collect', async r => {
-
                         // next image
                         if (r.emoji.name == '➡') {
                             image_index++;
-                            if (image_index > res.length - 1)
-                                image_index = 0;
+                            console.log(`url: ${res[image_index].url} | wxh: ${res[image_index].width}x${res[image_index].height}`);
                             await msg.edit(new Discord.MessageEmbed()
                                 .setTitle(`Resultados para **${fullmsg}** (${image_index + 1}/${res.length})`)
-                                .setImage(res[image_index].url)
+                                .setImage(await res[image_index].url)
                                 .setColor("#00FF00"));
                         }
+
 
                         // previous image
                         if (r.emoji.name == '⬅') {
                             image_index--;
-                            if (image_index < 0)
-                                try {
-                                    image_index = res.length - 1;
-                                } catch (e) {
-                                    image_index++;
-                                }
+                            console.log(`url: ${res[image_index].url} | wxh: ${res[image_index].width}x${res[image_index].height}`);
                             await msg.edit(new Discord.MessageEmbed()
                                 .setTitle(`Resultados para **${fullmsg}** (${image_index + 1}/${res.length})`)
-                                .setImage(res[image_index].url)
+                                .setImage(await res[image_index].url)
                                 .setColor("#00FF00"));
                         }
 
                         msg.reactions.removeAll();
-                        msg.react('⬅');
-                        msg.react('➡');
+                        if (image_index != 0)
+                            msg.react('⬅');
+
+                        if (image_index != res.length)
+                            msg.react('➡');
                     });
 
                     reactions.on('end', r => {
                         msg.reactions.removeAll();
                     })
-                });
-            }
+
+                }
+            })
         })
     }
 }
 
 module.exports.help = {
-    name: 'img',
+    name: 'image',
     descr: 'Faz uma busca no google imagens e entrega a primeira imagem',
     arg: ['busca']
 }
