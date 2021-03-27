0aa747f0ba29ddaeee576b1e563ac173b59bfb69
Pedro Henrique <silvapedro55@gmail.com>
2021-03-27 17:32:54 -0300

    rearranged image files

--- commands/fun/bogue.js
+++ commands/fun/bogue.js
@@ -2,16 +2,16 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
     switch (args[0]) {
         case 'prime':
-            await message.channel.send(new Discord.MessageAttachment('bogueprime.jpg'));
+            await message.channel.send(new Discord.MessageAttachment('./imagebank/bogueprime.jpg'));
             break;
         case 'pixel':
-            await message.channel.send(new Discord.MessageAttachment('boguepixel.jpg'));
+            await message.channel.send(new Discord.MessageAttachment('./imagebank/boguepixel.jpg'));
             break;
         case 'line':
-            await message.channel.send(new Discord.MessageAttachment('bogueline.png'));
+            await message.channel.send(new Discord.MessageAttachment('./imagebank/bogueline.png'));
             break;
         default: {
-            await message.channel.send(new Discord.MessageAttachment('bogue.jpg'));
+            await message.channel.send(new Discord.MessageAttachment('./imagebank/bogue.jpg'));
             break;
         }
     }
--- commands/fun/boleta.js
+++ commands/fun/boleta.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-    return message.channel.send(new Discord.MessageAttachment('boleta.jpeg'));
+    return message.channel.send(new Discord.MessageAttachment('./imagebank/boleta.jpeg'));
 }
 
 module.exports.help = {
