ce66303570c7f0ed8257f5860c552c3c01376b90
Pedro Henrique <silvapedro55@gmail.com>
2021-04-01 23:21:56 -0300

    added constraint to admin usage and updated script

--- commands/admin/runscript.js
+++ commands/admin/runscript.js
@@ -2,16 +2,22 @@ const Discord = require("discord.js");
 const botconfig = require.main.require("./botconfig.json");
 const main = require('../../BogueBot.js');
 module.exports.run = async (bot, message, args) => {
-    let current_servers = bot.guilds.cache.array();
-    for (let i = 0; i < current_servers.length; i++) {
-        try {
-            main.db.query('INSERT INTO guild(id, prefix) VALUES($1, $2)', [current_servers[i].id, botconfig.prefix])
-                .then(() => console.log(`added guild id [${current_servers[i].id}] to database sucessfully.`));
-
-        } catch (e) {
-            console.log(`error adding guild id [${current_servers[i].id}] to database: ${e}`);
+    if (message.author.id === process.env.admin_id) {
+        let current_servers = bot.guilds.cache.array();
+        for (let i = 0; i < current_servers.length; i++) {
+            try {
+                await main.db.query('UPDATE guild SET name=$1 WHERE id=$2', [current_servers[i].name, current_servers[i].id]);
+            } catch (e) {
+                console.log(`error naming guild id:${current_servers[i].id}`);
+            }
         }
+    } else {
+        return message.channel.send(new Discord.MessageEmbed()
+            .setTitle('Permissão insuficiente para executar o comando.')
+            .setDescription('Apenas desenvolvedores tem acesso a esse comando.')
+            .setColor('#FF0000'));
     }
+
 }
 
 module.exports.help = {
